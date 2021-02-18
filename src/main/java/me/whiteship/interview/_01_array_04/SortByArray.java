5317a4a747383a83673d5d281fdb1398bfcc3cba
smpark1020 <smpark911020@gmail.com>
2021-02-18 23:12:46 +0900

    Array Quiz 4

--- src/main/java/me/whiteship/interview/_01_array_04/SortByArray.java
+++ src/main/java/me/whiteship/interview/_01_array_04/SortByArray.java
@@ -1,15 +1,10 @@
 package me.whiteship.interview._01_array_04;
 
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 
 public class SortByArray {
 
     public static void main(String[] args) {
-        List<String> names = new LinkedList<>();
-        names.add("keesun");
-
         SortByArray sortByArray = new SortByArray();
         System.out.println(Arrays.toString(sortByArray.solution(new int[]{2, 4, 1, 5, 6, 9})));
     }
@@ -21,6 +16,18 @@ public class SortByArray {
      * @return
      */
     private int[] solution(int[] numbers) {
+    	boolean[] booleans = new boolean[100];
+    	for (int num : numbers) {
+    		booleans[num] = true;
+    	}
+    	
+    	int index = 0;
+    	for (int i = 0; i < booleans.length; i++) {
+    		if (booleans[i]) {
+    			numbers[index++] = i;
+    		}
+    	}
+    	
         return numbers;
     }
 
