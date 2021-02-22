4182bfc051372e103fa337f5752a91b56673467d
smith <crm90024@lotte.net>
2021-02-22 12:12:10 +0900

    update bubble sort

--- src/main/java/me/whiteship/interview/_01_array_04/BubbleSort.java
+++ src/main/java/me/whiteship/interview/_01_array_04/BubbleSort.java
@@ -11,9 +11,9 @@ public class BubbleSort {
 
 	private static int[] sort(int[] arr) {
 		for (int i = 0; i < arr.length - 1; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] > arr[j]) {
-					swapValue(arr, i, j);
+			for (int j = 1; j < arr.length - i; j++) {
+				if (arr[j - 1] > arr[j]) {
+					swapValue(arr, j - 1, j);
 				}
 			}
 		}
