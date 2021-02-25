ad95e853911dc3a37bb65b7f758dfd6381f62a0b
smpark1020 <smpark911020@gmail.com>
2021-02-25 23:33:43 +0900

    Infix to postfix

--- src/main/java/me/whiteship/interview/_03_stack_03/InfixToPostfix.java
+++ src/main/java/me/whiteship/interview/_03_stack_03/InfixToPostfix.java
@@ -1,5 +1,7 @@
 package me.whiteship.interview._03_stack_03;
 
+import java.util.Stack;
+
 public class InfixToPostfix {
 
     public static void main(String[] args) {
@@ -9,11 +11,49 @@ public class InfixToPostfix {
         System.out.println(infixToPostfix.convert("1+2*3").equals("123*+"));
     }
 
+    private int precedence (char c) {
+    	if (c == '+' || c == '/') {
+    		return 1;
+    	} else if (c == '*' || c == '/') {
+    		return 2;
+    	} else {
+    		return 0;
+    	}
+    }
+    
     /**
      * TODO 인픽스를 포스트픽스로 변환하기
      */
     private String convert(String infix) {
-        return infix;
+    	infix = infix.trim();
+    	StringBuilder postfix = new StringBuilder();
+    	Stack<Character> stack = new Stack<>();
+    	
+    	for (int i = 0; i < infix.length(); i++) {
+    		char c = infix.charAt(i);
+    		
+    		if (Character.isDigit(c)) {
+    			postfix.append(c);
+    		} else if (c == '(') {
+    			stack.push(c);
+    		} else if (c == ')') {
+    			while (!stack.isEmpty() && stack.peek() != '(') {
+    				postfix.append(stack.pop());
+    			}
+    			stack.pop();
+    		} else {
+    			while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())) {
+    				postfix.append(stack.pop());
+    			}
+    			stack.push(c);
+    		}
+    	}
+    	
+    	while (!stack.isEmpty()) {
+			postfix.append(stack.pop());
+		}
+    	
+    	return postfix.toString();
     }
 
 }
