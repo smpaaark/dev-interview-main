209ed66e02a76c08f8d7ec86c1379015b398cec1
smpark1020 <smpark911020@gmail.com>
2021-02-26 15:35:31 +0900

    Queue quiz 1

--- src/main/java/me/whiteship/interview/_04_queue_01/ReverseQueue.java
+++ src/main/java/me/whiteship/interview/_04_queue_01/ReverseQueue.java
@@ -2,6 +2,7 @@ package me.whiteship.interview._04_queue_01;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class ReverseQueue {
 
@@ -13,17 +14,39 @@ public class ReverseQueue {
 
         System.out.println(numbers);
         ReverseQueue reverseQueue = new ReverseQueue();
-        Queue<Integer> reversed = reverseQueue.reverse(numbers);
+        Queue<Integer> reversed = reverseQueue.reverse2(numbers);
         System.out.println(reversed);
     }
 
-    // TODO 큐가 제공하는 오퍼레이션만 사용해서 큐를 뒤집는 코드를 작성하라.
+    private Queue<Integer> reverse2(Queue<Integer> numbers) {
+		if (numbers.isEmpty()) {
+			return numbers;
+		}
+		
+    	int front = numbers.poll();
+    	numbers = reverse2(numbers);
+    	numbers.offer(front);
+    	
+		return numbers;
+	}
+
+
+	// TODO 큐가 제공하는 오퍼레이션만 사용해서 큐를 뒤집는 코드를 작성하라.
     //  제공하는 오퍼레이션은 다음과 같다.
     //  offer(E e): 큐의 끝에 데이터를 추가한다.
     //  E poll(): 큐 헤드를 꺼낸다.
     //  isEmpty(): 큐가 비어있는가.
     //  예) Q {1, 2, 3}  =>  Q {3, 2, 1}
     private Queue<Integer> reverse(Queue<Integer> numbers) {
+    	Stack<Integer> stack = new Stack<>();
+    	while (!numbers.isEmpty()) {
+    		stack.push(numbers.poll());
+    	}
+    	
+    	while (!stack.isEmpty()) {
+    		numbers.offer(stack.pop());
+    	}
+    	
         return numbers;
     }
 }
