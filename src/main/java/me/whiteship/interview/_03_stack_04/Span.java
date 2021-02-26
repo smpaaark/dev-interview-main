f67908dea47e877c8b719f2021af2d26702b6b2a
smpark1020 <smpark911020@gmail.com>
2021-02-26 10:10:21 +0900

    Stack quiz 4

--- src/main/java/me/whiteship/interview/_03_stack_04/Span.java
+++ src/main/java/me/whiteship/interview/_03_stack_04/Span.java
@@ -1,6 +1,7 @@
 package me.whiteship.interview._03_stack_04;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Span {
 
@@ -19,6 +20,20 @@ public class Span {
      * @return
      */
     private int[] solution(int[] price) {
-        return null;
+    	int[] span = new int[price.length];
+    	Stack<Integer> stack = new Stack<>();
+    	stack.push(0);
+    	span[0] = 1;
+    	
+    	for (int i  = 1; i < price.length; i++) {
+    		while (!stack.isEmpty() && price[stack.peek()] <= price[i]) {
+    			stack.pop();
+    		}
+    		
+    		span[i] = stack.isEmpty() ? i + 1 : i - stack.peek();
+    		stack.push(i);
+    	}
+    	
+    	return span;
     }
 }
